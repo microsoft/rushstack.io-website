4fd6eadc819d99bcf16710b66ff5ccdea0f8b386
Elliot Nelson <nelson.work@gmail.com>
2021-10-28 13:24:38 -0400

    Checkin default styled Docusaurus with Rushstack sidebars

---
+++ docusaurus.config.js
@@ -0,0 +1,153 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'My Site',
+  tagline: 'Dinosaurs are cool',
+  url: 'https://your-docusaurus-test-site.com',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'facebook', // Usually your GitHub org/user name.
+  projectName: 'docusaurus', // Usually your repo name.
+
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          routeBasePath: 'pages',
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl: 'https://github.com/facebook/docusaurus/edit/main/website/',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/facebook/docusaurus/edit/main/website/blog/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: '',
+        logo: {
+          alt: 'Rush Stack',
+          src: 'images/rushstack.svg',
+        },
+        items: [
+          {
+            type: 'doc',
+            docId: 'index',
+            position: 'right',
+            label: 'Docs',
+          },
+          {
+            type: 'doc',
+            docId: 'api/index',
+            position: 'right',
+            label: 'API',
+          },
+          {
+            type: 'doc',
+            docId: 'shop',
+            position: 'right',
+            label: 'Shop',
+          },
+          {
+            type: 'doc',
+            docId: 'news',
+            position: 'right',
+            label: 'News',
+          },
+          {
+            type: 'doc',
+            docId: 'contributing/get_started',
+            position: 'right',
+            label: 'GitHub',
+          },
+          {
+            type: 'doc',
+            docId: 'help/support',
+            position: 'right',
+            label: 'Help',
+          }
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: 'Tutorial',
+                to: '/docs/intro',
+              },
+            ],
+          },
+          {
+            title: 'Community',
+            items: [
+              {
+                label: 'Stack Overflow',
+                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+              },
+              {
+                label: 'Discord',
+                href: 'https://discordapp.com/invite/docusaurus',
+              },
+              {
+                label: 'Twitter',
+                href: 'https://twitter.com/docusaurus',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'Blog',
+                to: '/blog',
+              },
+              {
+                label: 'GitHub',
+                href: 'https://github.com/facebook/docusaurus',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+      algolia: {
+        appId: 'W2G1E3U5T0',
+        apiKey: 'a0ab6dfc3db0c301b0ca8e725af85641',
+        indexName: 'rushstack.io',
+        searchParameters: {
+          exclusionPatterns: [
+            'pages/api/*'
+          ]
+        }
+      }
+    }),
+};
+
+module.exports = config;
