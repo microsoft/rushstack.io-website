130ddc0825949703cd9254499d195cc2d79390c1
Pete Gonzalez <4673363+octogonz@users.noreply.github.com>
2021-12-19 20:04:20 -0800

    Add page for testing new community service

---
+++ experimental/test-login.js
@@ -0,0 +1,48 @@
+console.log('Starting test-login.js');
+
+const rootDiv = document.getElementById('root');
+
+if (!rootDiv) {
+  throw new Error('Root div not found');
+}
+
+var cookieMap = {};
+
+document.cookie
+  .split(';')
+  .map(function (part) {
+    return part.trim().split('=');
+  })
+  .forEach(function (pair) {
+    cookieMap[pair[0]] = decodeURIComponent(pair[1]);
+  });
+
+function htmlEncode(text) {
+  const tempDiv = document.createElement('div');
+  tempDiv.innerText = text;
+  return tempDiv.innerHTML;
+}
+
+var loggedInUser = (cookieMap['rscommunity-logged-in-user'] || '').trim();
+
+var serviceUrl = '';
+if (document.location.hostname.toUpperCase() !== 'LOCALHOST') {
+  serviceUrl = 'https://service.rushstack.io/';
+}
+var returnUrlQuery = 'returnUrl=' + encodeURIComponent(document.location.pathname);
+
+if (loggedInUser) {
+  // prettier-ignore
+  rootDiv.innerHTML = 'You are logged in as <b>' + htmlEncode(loggedInUser) + '</b>.'
+    + '<p>'
+    + '<a href="' + serviceUrl + '/logout?' + returnUrlQuery + '">'
+      + 'LOG OUT'
+    + '</a>';
+} else {
+  // prettier-ignore
+  rootDiv.innerHTML = 'You are not logged in'
+    + '<p>'
+    + '<a href="' + serviceUrl + '/login?' + returnUrlQuery + '">'
+      + 'LOG IN'
+    + '</a>';
+}
